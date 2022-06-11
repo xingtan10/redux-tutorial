59d14d0afdcf3d6cdf7ef9717fafd662778798e4
xingtan10 <terrytan1979@gmail.com>
2022-06-11 12:39:12 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 import Modal from './components/Modal';
 function App() {
   const { cartItems, isLoading } = useSelector((store) => store.cart1);
-  const { isOpen } = useSelector((store) => store.modal);
+  const { isOpen } = useSelector((store) => store.modal1);
   const dispatch = useDispatch();
 
   useEffect(() => {
