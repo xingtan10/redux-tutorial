e86037b8b3b6cb2b2e63101ec497d9c52002e21d
xingtan10 <terrytan1979@gmail.com>
2022-06-11 12:35:52 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 import { useEffect } from 'react';
 import Modal from './components/Modal';
 function App() {
-  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart1);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
