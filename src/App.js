8428ab0a0c96f51031e590652d24d01b0a2b774d
xingtan10 <terrytan1979@gmail.com>
2022-06-11 11:59:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
-import { useDispatch, useSelector } from "react-redux";
-import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
-import { useEffect } from "react";
-import Modal from "./Modal";
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
+import { useEffect } from 'react';
+import Modal from './components/Modal';
 function App() {
-  const { cartItems, isLoading } = useSelector((store) => store.cart1);
-  const { isOpen } = useSelector((store) => store.modal1);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,15 +16,16 @@ function App() {
 
   useEffect(() => {
     dispatch(getCartItems());
-  });
+  }, []);
 
   if (isLoading) {
     return (
-      <div className="loading">
+      <div className='loading'>
         <h1>Loading...</h1>
       </div>
     );
   }
+
   return (
     <main>
       {isOpen && <Modal />}
